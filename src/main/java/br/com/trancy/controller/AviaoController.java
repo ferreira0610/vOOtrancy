a2adf9b6ea121a2c4243931bfdf07cbfc98c97d1
roch-afel <rafael.rocha@dcomp.ufs.br>
2025-08-14 14:47:25 -0300

    Primeira parte da estrutura de pastas usando DAO e MVC

---
+++ src/main/java/br/com/trancy/controller/AviaoController.java
@@ -0,0 +1,5 @@
+package vOOtrancy.src.main.java.br.com.trancy.controller;
+
+public class AviaoController {
+    
+}
---
+++ src/main/java/br/com/trancy/model/dao/AviaoDAO.java
@@ -0,0 +1,5 @@
+package vOOtrancy.src.main.java.br.com.trancy.model.dao;
+
+public class AviaoDAO {
+    
+}
